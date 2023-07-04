ced4a07368072052159264dffd98bec9a24c063e
Sylina Zhang <abbiezsy@gmail.com>
2023-07-04 18:03:49 +0800

    split-chunk

--- blockchainetl/jobs/exporters/kinesis_item_exporter.py
+++ blockchainetl/jobs/exporters/kinesis_item_exporter.py
@@ -24,6 +24,7 @@ import json
 import typing as t
 import uuid
 from itertools import zip_longest
+import math
 
 import boto3
 
@@ -35,6 +36,7 @@ def _uuid_partition_key(_: dict) -> str:
 
 
 class KinesisItemExporter:
+    
     def __init__(
             self,
             stream_name: str,
@@ -44,6 +46,7 @@ class KinesisItemExporter:
         self._stream_name = stream_name
         self._partition_key_callable = partition_key_callable
         self._kinesis_client = None  # initialized in .open
+        self.KINESIS_SIZE_LIMIT = 1048576 # actually, it is 1048576
 
     def open(self) -> None:
         self._kinesis_client = boto3.client('kinesis')
@@ -55,16 +58,30 @@ class KinesisItemExporter:
             fillvalue=sentinel,
         )
         for chunk in chunks:
-            KINESIS_SIZE_LIMIT = 1048576 # actually, it is 1048576
             data = json.dumps(chunk, default=str)
             data_size = len(data.encode("utf-8"))
-            file_bigblock = open('~/bigblock.txt', 'w')
-            i = 0
+            # file_bigblock = open('~/bigblock.txt', 'w')
+            # i = 0
 
             if data_size > KINESIS_SIZE_LIMIT:
-                file_bigblock.write( i + " " + data + "\n")
-                i = i+1
+                print("-----------------------%s----------------------" % str(data_size))
+                new_chunks = self.split_chunk(chunk)
+                for small_chunk in new_chunks:
+                    self._kinesis_client.put_records(
+                        StreamName=self._stream_name,
+                        Records=[
+                            {
+                                'Data': _serialize_item(item),
+                                'PartitionKey': self._partition_key_callable(item),
+                            }
+                            for item in small_chunk
+                            if item is not sentinel
+                        ],
+                    )
                 continue
+                # file_bigblock.write( i + " " + data + "\n")
+                # i = i+1
+                # continue
             self._kinesis_client.put_records(
                 StreamName=self._stream_name,
                 Records=[
@@ -87,6 +104,29 @@ class KinesisItemExporter:
     def close(self):
         pass
 
+    def split_chunk(chunk):
+        subchunks = [chunk]
+        results = []
+        while subchunks != []:
+            subchunk = subchunks[0]
+            items_size = len(subchunk)
+            left_chunk = subchunk[:int(items_size/2)]
+            right_chunk = subchunk[int(items_size/2):]
+            if len(json.dumps(left_chunk, default=str).encode("utf-8")) > self.KINESIS_SIZE_LIMIT:
+                subchunks.append(left_chunk)
+            else:
+                results.append(left_chunk)
+            if len(json.dumps(right_chunk, default=str).encode("utf-8")) > self.KINESIS_SIZE_LIMIT:
+                subchunks.append(right_chunk)
+            else:
+                results.append(right_chunk)
+            subchunks.pop(0)
+        return results
+
+
+
+
 
 def _serialize_item(item: dict) -> bytes:
     return json.dumps(item).encode()
+
